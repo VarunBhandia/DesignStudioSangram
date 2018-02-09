6bc9d913f65ab311a19d2bf3db7bf6a78a1980ab
Varun Bhandia <varunbhandia@gmail.com>
2018-02-09 23:33:41 +0530

    Team Styling

--- team.php
+++ team.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Sangram</title>
+    <title>Team</title>
     <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,6 +9,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/team.css">
     <style>
   </style>
 </head>
@@ -16,7 +17,40 @@
     <?php include("navbar.php"); ?>
     <div class="row">
         <div class="col-md-1"></div>
-        <div class="col-md-10"></div>
+        <div class="col-md-10">
+            <div class="teamheader">
+                TEAM MEMBERS
+            </div>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+            <div class="line"></div> 
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+    <div class="row padding5">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="thumbnail_team">
+                        <img src="img/" class="team_image">
+                        <center>
+                        <div class="team_content">
+                            <p class="name_team">Dr. G. D. Ransinchung R. N.</p>
+                            <p class="post_team">Chief Sports Advisor</p>
+                            <p class="post_team">+91 999-999-9999</p>
+                        </div>
+                            </center>
+                    </div>
+                </div>
+                <div class="col-md-4"></div>
+                <div class="col-md-4"></div>
+            </div>
+        </div>
         <div class="col-md-1"></div>
     </div>
     <?php include("footer.php"); ?>
