51482c98403604b917eb3c23c077ede73e0d00ca
Varun Bhandia <varunbhandia@gmail.com>
2018-02-09 23:46:38 +0530

    Events Page

--- events.php
+++ events.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Sangram</title>
+    <title>Team</title>
     <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,106 +9,146 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/team.css">
     <style>
   </style>
 </head>
 <body>
-    <div class="gradient">
-        <div class="row">
-            <div class="col-md-1"></div>
-            <div class="col-md-10">
-                <nav class="navbar">
-                    <div class="container-fluid">
-                        <div class="navbar-header" style="margin-top: 2em;">
-                          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>                        
-                          </button>
-                          <a href="#"><img src="img/logo.png" alt="Sangram'18 Logo" class="logo"> </a><a href="#"><img src="img/iitr_logo.png" alt="Sangram'18 Logo" class="iitr_logo"> </a>
-                        </div>
-                        <div class="collapse navbar-collapse" id="myNavbar">
-                            <ul class="nav navbar-nav navbar-right">
-                                <li class="nav-items"><a href="#" class="navbar_link">About Us</a></li>
-                                <li class="nav-items"><a href="#" class="navbar_link">Events</a></li>
-                                <li class="nav-items"><a href="#" class="navbar_link">Sponsors</a></li>
-                                <li class="nav-items"><a href="#" class="navbar_link">Contact Us</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-            <div class="col-md-1"></div>
-        </div>
-    </div>
-    <div class="footer">
-        <div class="row">
-            <div class="col-md-8 black1">
-                <div class="row">
-                    <div class="col-md-2"></div>
-                    <div class="col-md-8">
-                        <img src="img/combined.png" alt="sangram'18" class="footerlogo">
-                        <div class="row">
-                            <div class="col-md-3">
-                                <img src="img/fb.png" alt="Sangram facebook" class="socialicons">
-                            </div>
-                            <div class="col-md-3">                        
-                                <img src="img/youtube.png" alt="Sangram youtube" class="socialicons">
-                            </div>
-                            <div class="col-md-3">                        
-                                <img src="img/google.png" alt="Sangram google" class="socialicons">
-                            </div>
-                            <div class="col-md-3">                        .
-                                <img src="img/insta.png" alt="Sangram instagram" class="socialicons">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-2"></div>
-                </div>
-            </div>
-            <div class="col-md-4 black2">  
-                <div class="row">
-                    <div class="col-md-2"></div>
-                    <div class="col-md-8">
-                        <div class="contact">CONTACT</div>
-                        <div class="contact_underline"></div>
-                        <div class="row">
-                            <div class="col-md-3">
-                                <img src="img/location.png" class="side2_icons">
-                            </div>
-                            <div class="col-md-9">
-                                <div class="address">
-                                    Sangram Office, Hobbies Club<br>
-                                    IIT Roorkee, Roorkee<br>
-                                    Uttarakhand, 247667
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-3">
-                                <img src="img/email.png" class="side2_icons">
-                            </div>
-                            <div class="col-md-9">
-                                <div class="address">
-                                    sangram18.iitr@gmail.com
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-3">
-                                <img src="img/number.png" class="side2_icons">
-                            </div>
-                            <div class="col-md-9">
-                                <div class="address">
-                                    +91 999-999-9999                                
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-2"></div>
-                </div>
+    <?php include("navbar.php"); ?>
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+            <div class="teamheader">
+                TEAM MEMBERS
             </div>
         </div>
+        <div class="col-md-1"></div>
     </div>
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+            <div class="line"></div> 
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+    <div class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-10">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="thumbnail_team">
+                        <img src="img/" class="team_image">
+                        <center>
+                        <div class="team_content">
+                            <p class="name_team">Dr. G. D. Ransinchung R. N.</p>
+                            <p class="post_team">Chief Sports Advisor</p>
+                            <p class="post_team">+91 999-999-9999</p>
+                        </div>
+                            </center>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail_team">
+                        <img src="img/" class="team_image">
+                        <center>
+                        <div class="team_content">
+                            <p class="name_team">Dr. G. D. Ransinchung R. N.</p>
+                            <p class="post_team">Chief Sports Advisor</p>
+                            <p class="post_team">+91 999-999-9999</p>
+                        </div>
+                            </center>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail_team">
+                        <img src="img/" class="team_image">
+                        <center>
+                        <div class="team_content">
+                            <p class="name_team">Dr. G. D. Ransinchung R. N.</p>
+                            <p class="post_team">Chief Sports Advisor</p>
+                            <p class="post_team">+91 999-999-9999</p>
+                        </div>
+                            </center>
+                    </div>
+                </div>
+            </div>       <div class="row">
+                <div class="col-md-4">
+                    <div class="thumbnail_team">
+                        <img src="img/" class="team_image">
+                        <center>
+                        <div class="team_content">
+                            <p class="name_team">Dr. G. D. Ransinchung R. N.</p>
+                            <p class="post_team">Chief Sports Advisor</p>
+                            <p class="post_team">+91 999-999-9999</p>
+                        </div>
+                            </center>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail_team">
+                        <img src="img/" class="team_image">
+                        <center>
+                        <div class="team_content">
+                            <p class="name_team">Dr. G. D. Ransinchung R. N.</p>
+                            <p class="post_team">Chief Sports Advisor</p>
+                            <p class="post_team">+91 999-999-9999</p>
+                        </div>
+                            </center>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail_team">
+                        <img src="img/" class="team_image">
+                        <center>
+                        <div class="team_content">
+                            <p class="name_team">Dr. G. D. Ransinchung R. N.</p>
+                            <p class="post_team">Chief Sports Advisor</p>
+                            <p class="post_team">+91 999-999-9999</p>
+                        </div>
+                            </center>
+                    </div>
+                </div>
+            </div>       <div class="row">
+                <div class="col-md-4">
+                    <div class="thumbnail_team">
+                        <img src="img/" class="team_image">
+                        <center>
+                        <div class="team_content">
+                            <p class="name_team">Dr. G. D. Ransinchung R. N.</p>
+                            <p class="post_team">Chief Sports Advisor</p>
+                            <p class="post_team">+91 999-999-9999</p>
+                        </div>
+                            </center>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail_team">
+                        <img src="img/" class="team_image">
+                        <center>
+                        <div class="team_content">
+                            <p class="name_team">Dr. G. D. Ransinchung R. N.</p>
+                            <p class="post_team">Chief Sports Advisor</p>
+                            <p class="post_team">+91 999-999-9999</p>
+                        </div>
+                            </center>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail_team">
+                        <img src="img/" class="team_image">
+                        <center>
+                        <div class="team_content">
+                            <p class="name_team">Dr. G. D. Ransinchung R. N.</p>
+                            <p class="post_team">Chief Sports Advisor</p>
+                            <p class="post_team">+91 999-999-9999</p>
+                        </div>
+                            </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-1"></div>
+    </div>
+    <?php include("footer.php"); ?>
 </body>
 </html>
