9775659a8a65de3cf540687da82c197f8a54525c
Varun Bhandia <varunbhandia@gmail.com>
2018-02-09 22:34:45 +0530

    Navbar

---
+++ navbar.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Sangram</title>
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+  </style>
+</head>
+<body>
+        <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-10">
+                <nav class="navbar">
+                    <div class="container-fluid">
+                        <div class="navbar-header" style="margin-top: 2em;">
+                          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>                        
+                          </button>
+                          <a href="#"><img src="img/logo.png" alt="Sangram'18 Logo" class="logo"> </a><a href="#"><img src="img/iitr_logo.png" alt="Sangram'18 Logo" class="iitr_logo"> </a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="myNavbar">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class="nav-items"><a href="#" class="navbar_link">About Us</a></li>
+                                <li class="nav-items"><a href="#" class="navbar_link">Events</a></li>
+                                <li class="nav-items"><a href="#" class="navbar_link">Sponsors</a></li>
+                                <li class="nav-items"><a href="#" class="navbar_link">Contact Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+    </body>
+</html>
