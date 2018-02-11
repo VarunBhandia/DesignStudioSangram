24ace9212f282d9a8858db84cea82b34ac747e29
Varun Bhandia <varunbhandia@gmail.com>
2018-02-11 18:07:41 +0530

    Footer responsive and timer

--- index.php
+++ index.php
@@ -33,8 +33,8 @@
         </div>
     </div>
     <div class="row" >
-        <div class="col-md-3"></div>
-        <div class="col-md-6">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
         <div class="timer_div ">
             <center>
             <div class="col-md-3" style="padding: 30px;">
@@ -188,7 +188,7 @@ var x = setInterval(function() {
 </div>
         </div>
 <!--<div>-->
-        <div class="col-md-3"></div>
+        <div class="col-md-2"></div>
     
         </div>
     <div class="about">
