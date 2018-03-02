734473c1fa92e0e69d989fd80dd45da9354f6367
harshG97 <harsh180397@gmail.com>
2018-03-02 19:47:01 +0530

    events yoga rule book

--- events.php
+++ events.php
@@ -473,7 +473,7 @@ Regards</p>
                                 <div class="col-md-4"></div>
                                 <div class="col-md-4">
                                     <div class="rules_button">
-                                         <a href="https://docs.google.com/document/d/1uLvGwhJXfnQ_HBcyCBMe_ScdRapE7uRdbAG3Gxn0sB0/edit" target="_BLANK"><p class="rules_button_content">
+                                         <a href="https://drive.google.com/open?id=0B-y8qD0vQPIQUlRsR0pOMW9UN1htM1NCamt3dHVXcE9yT0d3" target="_BLANK"><p class="rules_button_content">
                                          View Rules
                                         </p></a> 
                                     </div>
