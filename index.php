c9b19af2d4c9479679cc817d4544bb531d891b91
Varun Bhandia <varunbhandia@gmail.com>
2018-03-03 15:31:07 +0530

    Navbar issues

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
         <div class="row">
             <div class="col-md-1"></div>
             <div class="col-md-10">
+<!--
                 <nav class="navbar-fixed-top">
                     <div class="gradient-background"></div>
 
@@ -46,6 +47,29 @@
                         </div>
                     </div>
                 </nav>
+-->
+                <nav class="navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>                        
+                  </button>
+                  <a class="navbar-brand" style="margin-top: -.5em; padding-left: 5vw;" href="#"><img src="img/cec-logo-c.png" class="new-logo"  />  </a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class="nav-items"><a href="index.php#about" class="navbar_link">ABOUT US</a></li>
+                                <li class="nav-items"><a href="events.php" class="navbar_link">EVENTS</a></li>
+                                <li class="nav-items"><a href="sponsors.php" class="navbar_link">SPONSORS</a></li>
+                                <li class="nav-items"><a href="team.php" class="navbar_link">TEAM</a></li>
+                                <li class="nav-items"><a href="index.php#footer_index" class="navbar_link" style="padding-right: 14em;">CONTACT US</a></li>
+                            </ul>
+                </div>
+              </div>
+        </nav>
+
             </div>
             <div class="col-md-1"></div>
 </div>
