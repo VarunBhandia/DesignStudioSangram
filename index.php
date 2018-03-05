1e4de81fa1c94cea9a8fa593d795a6ef2e4f40ff
Varun Bhandia <varunbhandia@gmail.com>
2018-03-05 21:00:36 +0530

    Navbar php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
         <div class="row">
             <div class="col-md-1"></div>
             <div class="col-md-10">
-                <nav class="navbar navbar-fixed-top">
+                <nav class="navbar-fixed-top">
                     <div class="gradient-background"></div>
 
                     <div class="container-fluid">
@@ -55,44 +55,42 @@
                           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                             <span class="icon-bar" style="background-color:white;"></span>
                             <span class="icon-bar" style="background-color:white;"></span>
-                            <span class="icon-bar" style="background-color:white;"></span>
+                            <span class="icon-bar" style="background-color:white;"></span>                        
                           </button>
                             <ul class="nav navbar-nav">
-                                <li class="leftlogo"><a href="index.php"><img src="img/logo.png" alt="Sangram'18 Logo" class="logo"> </a></li>
-                                <li class="vl"></li>
-                                <li class="leftlogo"><a href="#"><img src="img/iitr_logo.png" alt="Sangram'18 Logo" class="iitr_logo"> </a></li>
-                            </ul>
-                        </div>
+                            <li class="leftlogo"><a href="index.php"><img src="img/logo.png" alt="Sangram'18 Logo" class="logo"> </a></li>
+                            <li class="leftlogo"><a href="#"><img src="img/iitr_logo.png" alt="Sangram'18 Logo" class="iitr_logo"> </a></li>
+                        </ul>
+                                </div>
                         <div class="collapse navbar-collapse" id="myNavbar">
                             <ul class="nav navbar-nav navbar-right">
-                                <li class="nav-items"><a href="index.php#about" class="navbar_link">ABOUT US</a></li>
-                                <li class="nav-items"><a href="events.php" class="navbar_link">EVENTS</a></li>
-                                <li class="nav-items"><a href="sponsors.php" class="navbar_link">SPONSORS</a></li>
-                                <li class="nav-items"><a href="team.php" class="navbar_link">TEAM</a></li>
-                                <li class="nav-items"><a href="index.php#footer_index" class="navbar_link" style="padding-right: 14em;">CONTACT US</a></li>
+                                <li class=""><a href="index.php#about" class="navbar_link nav-items">ABOUT US</a></li>
+                                <li class=""><a href="events.php" class="navbar_link nav-items">EVENTS</a></li>
+                                <li class=""><a href="sponsors.php" class="navbar_link nav-items">SPONSORS</a></li>
+                                <li class=""><a href="team.php" class="navbar_link nav-items">TEAM</a></li>
+                                <li class=""><a href="index.php#footer_index" class="padding14 navbar_link nav-items">CONTACT US</a></li>
                             </ul>
                         </div>
                     </div>
                 </nav>
             </div>
             <div class="col-md-1"></div>
-        </div>
-        <script>
-            var isVisible = false;
-            $('#mybutton').click(function(){
-                $(window).scrollTop(0);
-            });
-            $(window).scroll(function(){
-                 var shouldBeVisible = $(window).scrollTop()>200;
-                 if (shouldBeVisible && !isVisible) {
-                      isVisible = true;
-                      $('.gradient-background').show();
-                 } else if (isVisible && !shouldBeVisible) {
-                      isVisible = false;
-                      $('.gradient-background').hide();
-                }
-            });
-        </script>
+</div>
+<script>
+var isVisible = false;
+$('#mybutton').click(function(){
+    $(window).scrollTop(0);
+});
+$(window).scroll(function(){
+     var shouldBeVisible = $(window).scrollTop()>200;
+     if (shouldBeVisible && !isVisible) {
+          isVisible = true;
+          $('.gradient-background').show();
+     } else if (isVisible && !shouldBeVisible) {
+          isVisible = false;
+          $('.gradient-background').hide();
+    }
+});</script>
         <video autoplay muted loop class="myVideo">
           <source src="img/animated.mp4" type="video/mp4">
           Your browser does not support HTML5 video.
--- navbar.php
+++ navbar.php
@@ -10,21 +10,20 @@
                           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                             <span class="icon-bar" style="background-color:white;"></span>
                             <span class="icon-bar" style="background-color:white;"></span>
-                            <span class="icon-bar" style="background-color:white;"></span>
+                            <span class="icon-bar" style="background-color:white;"></span>                        
                           </button>
                             <ul class="nav navbar-nav">
                             <li class="leftlogo"><a href="index.php"><img src="img/logo.png" alt="Sangram'18 Logo" class="logo"> </a></li>
-                            <li class="vl"></li>
                             <li class="leftlogo"><a href="#"><img src="img/iitr_logo.png" alt="Sangram'18 Logo" class="iitr_logo"> </a></li>
                         </ul>
                                 </div>
                         <div class="collapse navbar-collapse" id="myNavbar">
                             <ul class="nav navbar-nav navbar-right">
-                                <li class="nav-items"><a href="index.php#about" class="navbar_link">ABOUT US</a></li>
-                                <li class="nav-items"><a href="events.php" class="navbar_link">EVENTS</a></li>
-                                <li class="nav-items"><a href="sponsors.php" class="navbar_link">SPONSORS</a></li>
-                                <li class="nav-items"><a href="team.php" class="navbar_link">TEAM</a></li>
-                                <li class="nav-items"><a href="index.php#footer_index" class="navbar_link" style="padding-right: 14em;">CONTACT US</a></li>
+                                <li class=""><a href="index.php#about" class="navbar_link nav-items">ABOUT US</a></li>
+                                <li class=""><a href="events.php" class="navbar_link nav-items">EVENTS</a></li>
+                                <li class=""><a href="sponsors.php" class="navbar_link nav-items">SPONSORS</a></li>
+                                <li class=""><a href="team.php" class="navbar_link nav-items">TEAM</a></li>
+                                <li class=""><a href="index.php#footer_index" class="padding14 navbar_link nav-items">CONTACT US</a></li>
                             </ul>
                         </div>
                     </div>
@@ -32,3 +31,37 @@
             </div>
             <div class="col-md-1"></div>
 </div>
+<!--
+<div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-10">
+                <nav class="navbar-fixed-top">
+                    <div class="gradient-background"></div>
+
+                    <div class="container-fluid">
+                        <div class="navbar-header" style="margin-top: 1em;">
+                          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="icon-bar" style="background-color:white;"></span>
+                            <span class="icon-bar" style="background-color:white;"></span>
+                            <span class="icon-bar" style="background-color:white;"></span>                        
+                          </button>
+                            <ul class="nav navbar-nav">
+                            <li class="leftlogo"><a href="index.php"><img src="img/logo.png" alt="Sangram'18 Logo" class="logo"> </a></li>
+                            <li class="leftlogo"><a href="#"><img src="img/iitr_logo.png" alt="Sangram'18 Logo" class="iitr_logo"> </a></li>
+                        </ul>
+                                </div>
+                        <div class="collapse navbar-collapse" id="myNavbar">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class=""><a href="index.php#about" class="navbar_link nav-items">ABOUT US</a></li>
+                                <li class=""><a href="events.php" class="navbar_link nav-items">EVENTS</a></li>
+                                <li class=""><a href="sponsors.php" class="navbar_link nav-items">SPONSORS</a></li>
+                                <li class=""><a href="team.php" class="navbar_link nav-items">TEAM</a></li>
+                                <li class=""><a href="index.php#footer_index" class="padding14 navbar_link nav-items">CONTACT US</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div class="col-md-1"></div>
+</div>
+-->
