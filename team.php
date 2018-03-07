d4e926c1e7d1e8a8c2deb8095627f4de067f78cf
Varun Bhandia <varunbhandia@gmail.com>
2018-03-07 15:30:29 +0530

    navbar and sumit

--- team.php
+++ team.php
@@ -317,7 +317,7 @@
               <div class="col-md-12 col-xs-12">
                 <center>
                   <div class="thumbnail_team">
-                      <img src="img/sumit.png" style="width:100%;height: 75%;" class="team_image">
+                      <img src="img/sumit.png" style="width:100%;height: 60%;" class="team_image">
                       <center>
                       <div class="team_content">
                           <p class="name_team">Sumit Yadav</p>
