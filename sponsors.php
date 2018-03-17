52d17aecd96cfc6b0e794ed3bb71343131a3b7ac
Varun Bhandia <varunbhandia@gmail.com>
2018-03-18 00:09:21 +0530

    spons deletions

--- sponsors.php
+++ sponsors.php
@@ -26,35 +26,6 @@
     <div class="row" style="margin-top: 20px;">
         <div class="col-md-1"></div>
         <div class="col-md-10">
-<!--
-             <div class="row">
-        <div class="col-md-5"></div>
-        <div class="col-md-2">
-            <div class="sponType">Title Sponsor</div> 
-        </div>
-        <div class="col-md-5"></div>
-    </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="spons_img">
-
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="spons_img">
-                        <img src="img/ongc.png" class="image_spons">
-
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="spons_img">
-
-                    </div>
-                </div>
-            </div>  
-            
--->
-            
              <div class="row">
         <div class="col-md-5"></div>
         <div class="col-md-2">
@@ -66,27 +37,8 @@
                 
                  <div class="col-md-4">
                      <div class="spons_img">
-<!--                        <img src="img/pjf.png" class="image_spons">-->
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Vardan Bapna</p>
-                            <p class="post_team">Head Admin &amp; Events</p>
-                            <p class="post_team">+91 976-083-5380</p>
-                        </div>
-                            </center>
--->
                          </div>
                 </div>
-<!--
-                <div class="col-md-4">
-                    <div class="spons_img">
-                        <img src="img/nescafe.png" class="image_spons">
-
-
-                    </div>
-                </div>
--->
                 <div class="col-md-4">
                     <div class="spons_img">
                         <a href="plb.php"><img src="img/patiala.png" class="image_spons"></a>
@@ -95,78 +47,9 @@
                 </div>
                 <div class="col-md-4">
                      <div class="spons_img">
-<!--                        <img src="img/pjf.png" class="image_spons">-->
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Vardan Bapna</p>
-                            <p class="post_team">Head Admin &amp; Events</p>
-                            <p class="post_team">+91 976-083-5380</p>
-                        </div>
-                            </center>
--->
                          </div>
                 </div>
             </div>
-            
-<!--
-                <div class="row">
-        <div class="col-md-5"></div>
-        <div class="col-md-2">
-            <div class="sponType">Food Partner</div> 
-        </div>
-        <div class="col-md-5"></div>
-    </div>
-            <div class="row">
-                <div class="col-md-4">
-                     <div class="spons_img">
-                        <img src="img/layer-1.png" class="image_spons">
-
-                         </div>
-                </div>
-                <div class="col-md-4">
-                     <div class="spons_img">
-                        <img src="img/pizza-hut.png" class="image_spons">
-
-                         </div>
-                </div>
-                <div class="col-md-4">
-                   <div class="spons_img">
-                        <img src="img/spice-club.png" class="image_spons">
-
-                       </div>
-                </div>
-            </div>
--->
-            
-<!--
-                   <div class="row">
-        <div class="col-md-5"></div>
-        <div class="col-md-2">
-            <div class="sponType">Fashion Partner</div> 
-        </div>
-        <div class="col-md-5"></div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="spons_img">
-
-                        </div>
-                </div>
-                <div class="col-md-4">
-                     <div class="spons_img">
-                        <img src="img/51-m-vc-ldgt-l-sx-425.png" class="image_spons">
-
-                        </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="spons_img">
-
-                        </div>
-                </div>
-            </div>
--->
-                       
                      <div class="row">
         <div class="col-md-5"></div>
         <div class="col-md-2">
@@ -177,44 +60,15 @@
             <div class="row">
                 <div class="col-md-4">
                     <div class="spons_img">
-<!--                        <img src="img/sbi-bank-1.png" class="image_spons" >-->
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Sumit Yadav</p>
-                            <p class="post_team">Organising Secretory</p>
-                            <p class="post_team">+91 80064-78565</p>
-                        </div>
-                            </center>
--->
                         </div>
                 </div>
                 <div class="col-md-4">
                      <div class="spons_img">
                         <img src="img/heelium.png" class="image_spons">
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Sambit Sarkar</p>
-                            <p class="post_team">Treasurer</p>
-                            <p class="post_team">+91 87551-82329</p>
-                        </div>
-                            </center>
--->
                         </div>
                 </div>
                 <div class="col-md-4">
                     <div class="spons_img">
-<!--                        <img src="img/c-6-jsoekt-1394471158331.png" class="image_spons">-->
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Vipul Seth</p>
-                            <p class="post_team">Co-Treasurer</p>
-                            <p class="post_team">+91 94134-08986</p>
-                        </div>
-                            </center>
--->
                         </div>
                 </div>
             </div>
@@ -232,43 +86,19 @@
                 <div class="col-md-4">
                     <div class="spons_img">
                         <img src="img/cry.png" class="image_spons" >
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Sumit Yadav</p>
-                            <p class="post_team">Organising Secretory</p>
-                            <p class="post_team">+91 80064-78565</p>
-                        </div>
-                            </center>
--->
+
                         </div>
                 </div>
                 <div class="col-md-4">
                      <div class="spons_img">
                         <img src="img/divyanglogo.png" class="image_spons">
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Sambit Sarkar</p>
-                            <p class="post_team">Treasurer</p>
-                            <p class="post_team">+91 87551-82329</p>
-                        </div>
-                            </center>
--->
+
                         </div>
                 </div>
                 <div class="col-md-4">
                     <div class="spons_img">
                         <img src="img/pjf.png" class="image_spons">
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Vipul Seth</p>
-                            <p class="post_team">Co-Treasurer</p>
-                            <p class="post_team">+91 94134-08986</p>
-                        </div>
-                            </center>
--->
+
                         </div>
                 </div>
             </div>
@@ -283,58 +113,23 @@
             <div class="row">
                 <div class="col-md-2">
                     <div class="spons_img">
-<!--                        <img src="img/sbi-bank-1.png" class="image_spons" >-->
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Sumit Yadav</p>
-                            <p class="post_team">Organising Secretory</p>
-                            <p class="post_team">+91 80064-78565</p>
-                        </div>
-                            </center>
--->
-                        </div>
+</div>
                 </div>
                 <div class="col-md-4">
                      <div class="spons_img">
                         <img src="img/sisco-research-laboratories-pvt-ltd.png" class="image_spons">
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Sambit Sarkar</p>
-                            <p class="post_team">Treasurer</p>
-                            <p class="post_team">+91 87551-82329</p>
-                        </div>
-                            </center>
--->
+
                         </div>
                 </div>
                 <div class="col-md-4">
                     <div class="spons_img">
                         <img src="img/c-6-jsoekt-1394471158331.png" class="image_spons">
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Vipul Seth</p>
-                            <p class="post_team">Co-Treasurer</p>
-                            <p class="post_team">+91 94134-08986</p>
-                        </div>
-                            </center>
--->
+
                         </div>
                 </div>
                  <div class="col-md-2">
                     <div class="spons_img">
-<!--                        <img src="img/sbi-bank-1.png" class="image_spons" >-->
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Sumit Yadav</p>
-                            <p class="post_team">Organising Secretory</p>
-                            <p class="post_team">+91 80064-78565</p>
-                        </div>
-                            </center>
--->
+
                         </div>
                 </div>
             </div>
@@ -350,7 +145,6 @@
             <div class="row">
                 <div class="col-md-4">
                     <div class="spons_img">
-<!--                        <img src="img/ongc.png" class="image_spons">-->
 
                     </div>
                 </div>
@@ -362,42 +156,11 @@
                 </div>
                 <div class="col-md-4">
                     <div class="spons_img">
-<!--                        <img src="img/pizza-hut.png" class="image_spons">-->
 
                     </div>
                 </div>
             </div>  
-<!--
-             <div class="row">
-        <div class="col-md-5"></div>
-        <div class="col-md-2">
-            <div class="sponType">Online Banking Partner</div> 
-        </div>
-        <div class="col-md-5"></div>
-    </div>
--->
-    
-<!--
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="spons_img">
 
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="spons_img">
-                        <img src="img/sbi-bank-1.png" class="image_spons">
-
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="spons_img">
-
-                    </div>
-                </div>
-            </div>  
--->
-            
         </div>
         <div class="col-md-1"></div>
     </div>
@@ -428,30 +191,13 @@
                  <div class="col-md-4">
                      <div class="spons_img">
                          <img src="img/nescafe.png" class="image_spons">
-<!--                        <img src="img/pjf.png" class="image_spons">-->
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Vardan Bapna</p>
-                            <p class="post_team">Head Admin &amp; Events</p>
-                            <p class="post_team">+91 976-083-5380</p>
-                        </div>
-                            </center>
--->
+
                          </div>
                 </div>
                 <div class="col-md-4">
                     <div class="spons_img">
                         <img src="img/pizza-hut.png" class="image_spons">
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Harsh Bansal</p>
-                            <p class="post_team">Head Admin &amp;Events</p>
-                            <p class="post_team">+91 721-725-9085</p>
-                        </div>
-                            </center>
--->
+
                     </div>
                 </div>
                 <div class="col-md-4">
@@ -469,30 +215,13 @@
                  <div class="col-md-4">
                      <div class="spons_img">
                          <img src="img/ongc.png" class="image_spons">
-<!--                        <img src="img/pjf.png" class="image_spons">-->
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Vardan Bapna</p>
-                            <p class="post_team">Head Admin &amp; Events</p>
-                            <p class="post_team">+91 976-083-5380</p>
-                        </div>
-                            </center>
--->
+
                          </div>
                 </div>
                 <div class="col-md-4">
                     <div class="spons_img">
                         <img src="img/sbi-bank-1.png" class="image_spons">
-<!--
-                        <center>
-                        <div class="team_content">
-                            <p class="name_team">Harsh Bansal</p>
-                            <p class="post_team">Head Admin &amp;Events</p>
-                            <p class="post_team">+91 721-725-9085</p>
-                        </div>
-                            </center>
--->
+
                     </div>
                 </div>
                 <div class="col-md-4">
