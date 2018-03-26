71e9735b3bdfbb1d77ba7afda0cba564ff39775b
harshG97 <harsh180397@gmail.com>
2018-03-26 22:40:06 +0530

    live scores integration

---
+++ Squash.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Live Scores</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600,900" rel="stylesheet">
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <meta charset="utf-8">
+    <meta name="description" content="Sangram boasts of being a one of a kind festival hosted by IIT Roorkee and is a four day sports extravaganza. It brings out the sporting spirit of the brilliant minds of the country and provides an ideal platform for all the sports enthusiasts to compete with each other.">
+    <meta name="keywords" content="sports, sports fest, engineering, study, fest, engineers, technical, iit ,iit roorkee ,roorkee ,uttrakhand ,Varun Bhandia ,sangram18 ,sangramiitr.in">
+    <meta name="author" content="Varun Bhandia">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/team.css">
+    <link rel="stylesheet" href="css/events.css">
+    <style>
+  </style>
+
+
+</head>
+<body>
+    <?php include("navbar.php"); ?>
+    <div class="container">
+        <div style="margin-top:10em;">
+         
+        </div>
+
+        
+    </div>
+    
+     <?php include("footer.php"); ?>
+</body>
+</html>
