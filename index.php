d4b69122c50dfc02b49ee85b10d049deb7b731bf
ritikkhatri <ritikkhatri876@gmail.com>
2019-02-14 00:11:40 +0530

    new spons and video

--- index.php
+++ index.php
@@ -17,53 +17,7 @@
         <link rel="stylesheet" href="css/hover.css">
     </head>
     <body>
-        <div class="row">
-            <div class="col-md-1"></div>
-            <div class="col-md-10">
-                <nav class="navbar-fixed-top index">
-                    <div class="gradient-background_index"></div>
-
-                    <div class="container-fluid">
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                                <span class="icon-bar" ></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>                        
-                            </button>
-                            <ul class="nav navbar-nav">
-                                <li class="leftlogo"><a href="index.php"><img src="img/logo.png" alt="Sangram'19 Logo" class="logo"> </a></li>
-                                <li class="leftlogo"><a href="index.php"><img src="img/iitr_logo.png" alt="Sangram'19 Logo" class="iitr_logo"> </a></li>
-                            </ul>
-                        </div>
-                        <div class="collapse1 navbar-collapse1" id="myNavbar">
-                            <ul class="nav navbar-nav navbar-right">
-                                <li class=""><a href="about_us.php" class="navbar_link_index nav-items">ABOUT US</a></li>
-                                <li class=""><a href="events.php" class="navbar_link_index nav-items">EVENTS</a></li>
-                                <li class=""><a href="sponsors.php" class="navbar_link_index nav-items">SPONSORS</a></li>
-                                <li class=""><a href="team.php" class="navbar_link_index nav-items">TEAM</a></li>
-                                <li class=""><a href="contact_us.php" class="padding14 navbar_link nav-items">CONTACT US</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-            <div class="col-md-1"></div>
-        </div>
-        <script>
-            var isVisible = false;
-            $('#mybutton').click(function(){
-                $(window).scrollTop(0);
-            });
-            $(window).scroll(function(){
-                var shouldBeVisible = $(window).scrollTop()>200;
-                if (shouldBeVisible && !isVisible) {
-                    isVisible = true;
-                    $('.gradient-background_index').show();
-                } else if (isVisible && !shouldBeVisible) {
-                    isVisible = false;
-                    $('.gradient-background_index').hide();
-                }
-            });</script>
+    <?php include("navbar.php"); ?>
 
         <video autoplay muted loop class="myVideo">
             <source src="img/animated.mp4" type="video/mp4">
--- sponsors.php
+++ sponsors.php
@@ -23,177 +23,21 @@
     </head>
     <body class="blackBody">
         <?php include("navbar.php"); ?>
- 
+ <div class="container sponsCont">
         <div class="row">
-            <div class="col-md-1"></div>
-            <div class="col-md-10">
-                <div class="teamheader" >
-                    PREVIOUS SPONSORS
-                </div>
+            <div class="col-md-3 ">
+                <img src="img/spons/ATKT-eng.png" class="spons_img">
             </div>
             <div class="col-md-1"></div>
-        </div>
-        <div class="row">
-            <div class="col-md-1"></div>
-            <div class="col-md-10">
-                <div class="line_team" ></div> 
+            <div class="col-md-3">
+            <img src="img/spons/WAC_current_logo.png" class="spons_img">
             </div>
             <div class="col-md-1"></div>
-        </div>
-            <div class="col-md-1"></div>
-            <div class="col-md-10">
-                <div class="row">
-                    <div class="col-md-5"></div>
-                    
-                        <div class="sponType" >Beverage Partner</div> 
-                    
-                    <div class="col-md-5"></div>
-                </div>
-                <div class="row">
-
-                    <div class="col-md-4">
-                        <div class="spons_img">
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="spons_img">
-                            <a href="plb.php"><img src="img/patiala.png" class="image_spons" alt="patiala"></a>
-
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="spons_img">
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-5"></div>
-                    
-                        <div class="sponType" >Social Initiatives Partner</div> 
-                    
-                    <div class="col-md-5"></div>
-                </div>
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="spons_img">
-                            <img src="img/cry.png" class="image_spons" >
-
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="spons_img">
-                            <img src="img/divyanglogo.png"  class="image_spons">
-
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="spons_img">
-                            <img src="img/pjf.png" class="image_spons">
-
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-5"></div>
-                    
-                        <div class="sponType" >Technology Partner</div> 
-                    
-                    <div class="col-md-5"></div>
-                </div>
-                <div class="row">
-                    <div class="col-md-2">
-                        <div class="spons_img">
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="spons_img">
-                            <img src="img/sisco-research-laboratories-pvt-ltd.png" class="image_spons">
-
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="spons_img">
-                            <img src="img/c-6-jsoekt-1394471158331.png"  class="image_spons">
-
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <div class="spons_img">
-
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-
-<div class="col-md-4">
-    <div class="spons_img">
-        <img src="img/ongc.png"  class="image_spons">
-
-    </div>
-</div>
-<div class="col-md-4">
-    <div class="spons_img">
-        <img src="img/sbi-bank-1.png" class="image_spons">
-
-    </div>
-</div>
-<div class="col-md-4">
-    <div class="spons_img">
-        <img src="img/51-m-vc-ldgt-l-sx-425.png" class="image_spons">
-
-    </div>
-</div>
-
-</div>
-
-        
+            <div class="col-md-3">
+            <img src="img/spons/logo-shiv.png" class="spons_img">
             </div>
-            <div class="col-md-1"></div>
-        </div>
-
-     
-
-
-        <div class="row" >
-            <div class="col-md-1"></div>
-            <div class="col-md-10">
-                <div class="row">
-                <div class="row">
-                    <div class="col-md-5"></div>
-                    
-                        <div class="sponType" >Food Partners</div> 
-                    
-                    <div class="col-md-5"></div>
-                </div>
-
-                    <div class="col-md-4">
-                        <div class="spons_img">
-                            <img src="img/nescafe.png" class="image_spons">
-
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="spons_img">
-                            <img src="img/pizza-hut.png" class="image_spons">
-
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="spons_img">
-                            <img src="img/spice-club.png"  class="image_spons">
-
-                        </div>
-                    </div>
-
-                </div>
-
-
-               
-            </div>
-            <div class="col-md-1"></div>
-        </div>
-
+</div>
+</div>
         <?php include("footer.php"); ?>
     </body>
 </html>
